b1deaf3abafbfc4eb958a8c845aae9072103a24d
Andrew Cooley <36833204+amc3777@users.noreply.github.com>
2025-03-10 13:08:43 -0500

    updated build script

--- build.sh
+++ build.sh
@@ -4,9 +4,20 @@
 PROJECT_ID="${PROJECT_ID:?PROJECT_ID environment variable not set.}"
 SERVICE_ACCOUNT="${SERVICE_ACCOUNT:?SERVICE_ACCOUNT environment variable not set.}"
 REGION="${REGION:?REGION environment variable not set.}"
-REPOSITORY_NAME="streamlit-repo" #or whatever your repo name is.
+REPOSITORY_NAME="streamlit-repo"
 IMAGE_NAME="streamlit-app"
 CONTAINER_IMAGE="${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPOSITORY_NAME}/${IMAGE_NAME}"
+
+if ! gcloud artifacts repositories describe "$REPOSITORY_NAME" --location="$LOCATION" --project="$PROJECT_ID" > /dev/null 2>&1; then
+  echo "Artifact Registry repository '$REPOSITORY_NAME' does not exist. Creating..."
+  gcloud artifacts repositories create "$REPOSITORY_NAME" \
+    --location="$REGION" \
+    --repository-format="DOCKER" \
+    --project="$PROJECT_ID" \
+    --description="Docker repository for my project"
+else
+  echo "Artifact Registry repository '$REPOSITORY_NAME' already exists."
+fi
 
 # Build the Docker Image
 echo "Building Docker image..."
